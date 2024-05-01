e78408d9d936f934e51e33e388843e8e4935f8c2
Yejiin21 <kyg9802@naver.com>
2024-05-01 13:42:08 +0900

    개발환경세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Main from "./pages/Main";
-import Login from "./pages/Login";
-import Layout from "./components/common/Layout";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './pages/layout';
+import Main from './pages/Main';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Main />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout />,
-      children: [
-        { path: "", element: <Main /> },
-        { path: "/login", element: <Login /> },
-      ],
-    },
-  ]);
   return <RouterProvider router={router} />;
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
