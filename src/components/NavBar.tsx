b2401bc1f596500d5a548ff089ab3b98ecd7fa00
Yejiin21 <kyg9802@naver.com>
2024-05-24 01:11:07 +0900

    NavBar 오류 해결

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,15 +4,16 @@ import Info from '../assets/navicon/Info.png';
 import Intake from '../assets/navicon/Intake.png';
 import Register from '../assets/navicon/Register.png';
 import Search from '../assets/navicon/Search.png';
+
 const NavBar: React.FC = () => {
   const NavStyle = {
     bottom: '0', // 화면 하단에 고정
-    width: '453px',
+    width: '100%',
     height: '4.25rem',
     borderTop: '1px solid #D9D9D9',
-    borderRadiusTop: '15px',
-    backgroundColor:"white",
-    
+    borderRadius: '15px 15px 0 0',
+    backgroundColor: 'white',
+    position: 'absolute',
   };
 
   const NavContentStyle = {
@@ -24,7 +25,7 @@ const NavBar: React.FC = () => {
   };
 
   return (
-    <nav className="flex absolute" style={NavStyle}>
+    <nav className="fixed flex bottom-0 w-full" style={NavStyle}>
       <div
         style={{
           ...NavContentStyle,
